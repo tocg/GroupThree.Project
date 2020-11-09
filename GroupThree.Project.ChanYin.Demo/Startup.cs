c7b19440ca0f199a712d010fa3868764ed13ce71
tocg <max.liang@lyetech.com>
2020-11-09 09:15:43 +0800

    aaaaaaa

--- GroupThree.Project.ChanYin.Demo/Startup.cs
+++ GroupThree.Project.ChanYin.Demo/Startup.cs
@@ -12,6 +12,10 @@ namespace GroupThree.Project.ChanYin.Demo
 {
     public class Startup
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
